68dc73e9f3cea39e9cdce9d02d6ad57e68e8312d
Hillary <hillarymuboka@gmail.com>
2026-05-24 19:33:11 +0300

    Add shop route group layout wrapping CartContext provider

---
+++ app/(shop)/layout.tsx
@@ -0,0 +1,5 @@
+import { CartProvider } from '@/components/shop/CartContext';
+
+export default function ShopLayout({ children }: { children: React.ReactNode }) {
+  return <CartProvider>{children}</CartProvider>;
+}
